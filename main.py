89afaee35e663b20b025162e724a6e5221d6874d
hyunsikhwang <gomting@naver.com>
2016-08-09 23:43:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import lxml.html
 
-url='http://finance.daum.net/quote/all.daum?type=S&stype=P'
+url = 'http://finance.daum.net/quote/all.daum?type=S&stype=P'
+url2 = 'http://dart.fss.or.kr/report/viewer.do?rcpNo=20160809800398&dcmNo=5249424&eleId=0&offset=0&length=0&dtd=HTML'
 
-html = lxml.html.parse(url)
+html = lxml.html.parse(url2)
 # packages = html.xpath('//tr/td/a/text()') # get the text inside all "<tr><td><a ...>text</a></td></tr>"
-packages = html.xpath('//table[@class="gTable clr"]/tr/td/span/text()') # get the text inside all "<tr><td><a ...>text</a></td></tr>"
+# packages = html.xpath('//table[@class="gTable clr"]/tr/td/span/text()') # get the text inside all "<tr><td><a ...>text</a></td></tr>"
 # print packages
-test=html.xpath("//tr/td/a/@href")
+
+packages = html.xpath('//table/tr/td/span/text()')
+# test = html.xpath("//tr/td/a/@href")
 
 A = {}
 i = 0
