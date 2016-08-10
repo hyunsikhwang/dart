7e8a8c3e79b33e95be2fe45c6ad90b1c5d2f0618
hyunsikhwang <gomting@naver.com>
2016-08-10 20:28:31 +0900

    Update bond.py

--- bond.py
+++ bond.py
@@ -1,24 +1,24 @@
-import sys
+import sys 
 from PyQt4.QtGui import * 
-from PyQt4.QtCore import *
-from PyQt4.QtWebKit import *
-import lxml.html
+from PyQt4.QtCore import * 
+from PyQt4.QtWebKit import * 
+from lxml import html
 
-class Render(QWebPage):
-  def __init__(self, url):
-    self.app = QApplication(sys.argv)
-    QWebPage.__init__(self)
-    self.loadFinished.connect(self._loadFinished)
-    self.mainFrame().load(QUrl(url))
-    self.app.exec_()
+class Render(QWebPage): 
+  def __init__(self, url): 
+    self.app = QApplication(sys.argv) 
+    QWebPage.__init__(self) 
+    self.loadFinished.connect(self._loadFinished) 
+    self.mainFrame().load(QUrl(url)) 
+    self.app.exec_() 
 
-  def _loadFinished(self, result):
-    self.frame = self.mainFrame()
+  def _loadFinished(self, result): 
+    self.frame = self.mainFrame() 
     self.app.quit()
 
-url = 'http://www.kofiabond.or.kr/websquare/websquare.html?w2xPath=/xml/bondint/lastrop/BISLastAskPrcDay.xml&amp;divisionId=MBIS01010010000000&amp;divisionNm=%25EC%259D%25BC%25EC%259E%2590%25EB%25B3%2584&amp;tabIdx=1&amp;w2xHome=/xml/&amp;w2xDocumentRoot='  
+url = 'http://www.kofiabond.or.kr/websquare/websquare.html?w2xPath=/xml/bondint/lastrop/BISLastAskPrcDay.xml&amp;divisionId=MBIS01010010000000&amp;divisionNm=%25EC%259D%25BC%25EC%259E%2590%25EB%25B3%2584&amp;tabIdx=1&amp;w2xHome=/xml/&amp;w2xDocumentRoot=' 
 #This does the magic.Loads everything
-r = Render(url)
+r = Render(url) 
 #result is a QString.
 result = r.frame.toHtml()
 
@@ -26,7 +26,7 @@ result = r.frame.toHtml()
 formatted_result = str(result.toAscii())
 
 #Next build lxml tree from formatted_result
-tree = lxml.html.fromstring(formatted_result)
+tree = html.fromstring(formatted_result)
 
 #Now using correct Xpath we are fetching URL of archives
 archive_links = tree.xpath('//table/tbody/tr/td/nobr/text()')
